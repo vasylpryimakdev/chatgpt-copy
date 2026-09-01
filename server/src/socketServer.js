c7121b21f5ceba82ef36ed722fc551b467cac8a8
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-09-01 17:27:39 +0300

    feat: handle chat sessions on socket server

--- server/src/socketServer.js
+++ server/src/socketServer.js
@@ -1,4 +1,7 @@
 const { Server } = require("socket.io");
+const { v4: uuid } = require("uuid");
+
+let sessions = {};
 
 const registerSocketServer = (server) => {
   const io = new Server(server, {
@@ -10,7 +13,44 @@ const registerSocketServer = (server) => {
 
   io.on("connection", (socket) => {
     console.log(`user connected ${socket.id}`);
+
+    socket.on("session-history", (data) => {
+      sessionHistoryHandler(socket, data);
+    });
+
+    socket.on("conversation-message", (data) => {
+      conversationMessageHandler(socket, data);
+    });
   });
+};
+
+const sessionHistoryHandler = (socket, data) => {
+  const { sessionId } = data;
+
+  if (sessions[sessionId]) {
+    // send existing session data back to user
+
+    socket.emit("session-details", {
+      sessionId,
+      conversations: sessions[sessionId],
+    });
+  } else {
+    const newSessionId = uuid();
+
+    sessions[newSessionId] = [];
+
+    const sessionDetails = {
+      sessionId: newSessionId,
+      conversations: [],
+    };
+
+    socket.emit("session-details", sessionDetails);
+  }
+};
+
+const conversationMessageHandler = (socket, data) => {
+  console.log("message came from client side");
+  console.log(data);
 };
 
 module.exports = { registerSocketServer };
